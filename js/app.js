2a0507ace0ad5c5f44bbfae6217ff6c043fe1778
matthewmjm <matthewmjm@yahoo.com>
2016-07-17 19:23:08 -0600

    jquery-streetfighter_before animation
    Unit 2 Lesson2.4 (before animation)

--- js/app.js
+++ js/app.js
@@ -6,5 +6,15 @@ $(document).ready(function () {
         .mouseleave(function () {
             $('.ryu-ready').hide();
             $('.ryu-still').show();
+        })
+        .mousedown(function () {
+            // play hadouken sound
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').show();
+        })
+        .mouseup(function () {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
         });
 });
