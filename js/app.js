8581fb0fc489793e2935f3720c85f51b37da0363
matthewmjm <matthewmjm@yahoo.com>
2016-07-18 22:44:46 -0600

    final jquery-streetfighter
    End of Unit 2, Lesson 2

--- js/app.js
+++ js/app.js
@@ -1,31 +1,84 @@
+/* Rules
+- hide everyting before you show anything
+- for binary actions the second action is always to return to the default state
+- when the page loads we can target the container directly; on mouse evenets we have to target the parent container first and then the child; for key events we target directly at the document level
+- before starting any animation make sure that any other animation is finished first
+- we stop any sound before we start playing another one
+*/
+
 $(document).ready(function () {
+
+    // when the page loads
+    $('.ryu-action').hide();
+    $('.ryu-still').show();
+
+
+    //when the mouse hovers over the guy
     $('.ryu').mouseenter(function () {
-            $('.ryu-still').hide();
-            $('.ryu-ready').show();
-        })
-        .mouseleave(function () {
-            $('.ryu-ready').hide();
-            $('.ryu-still').show();
-        })
-        .mousedown(function () {
-            playHadouken();
-            $('.ryu-ready').hide();
-            $('.ryu-throwing').show();
-            $('.hadouken').finish().show().animate({
-                'left': '1080px'
-            }, 500, function () {
-                $(this).hide();
-                $(this).css('left', '580px');
-            });
-        })
-        .mouseup(function () {
-            $('.ryu-throwing').hide();
-            $('.ryu-ready').show();
+        $('.ryu-action').hide();
+        $('.ryu-ready').show();
+    });
+    //mouse hovers of the guy
+    $('.ryu').mouseleave(function () {
+        $('.ryu-action').hide();
+        $('.ryu-still').show();
+    });
+
+
+    //when the mouse click the guy throws the thing
+    $('.ryu').mousedown(function () {
+        playHadouken();
+        $('.ryu-action').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate({
+            'left': '300px'
+        }, 500, function () {
+            $(this).stop();
+            $(this).hide();
+            $(this).css('left', '-212px');
         });
+    });
+    //when the is not clicked
+    $('.ryu').mouseup(function () {
+        $('.ryu-throwing').hide();
+        $('.ryu-still').show();
+    });
+
+
+    //press the X key so the guy strikes the pose and music plays
+    $(document).keydown(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-action').hide();
+            $('.ryu-cool').show();
+            playCool();
+        }
+    });
+    //release the X key the guy goes to ready pose and music stops
+    $(document).keyup(function (e) {
+        if (e.keyCode == 88) {
+            $('.ryu-action').hide();
+            $('.ryu-still').show();
+            $('#cool')[0].pause();
+        }
+    });
 });
 
+var hadoukenSound = false;
+
 function playHadouken() {
-    $('#hadouken-sound')[0].volume = 0.5;
-    $('#hadouken-sound')[0].load();
-    $('#hadouken-sound')[0].play();
+    hadoukenSound = !hadoukenSound;
+    if (hadoukenSound) {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
+}
+
+var coolSound = false;
+
+function playCool() {
+    coolSound = !coolSound;
+    if (coolSound) {
+        $('#cool')[0].play();
+    }
 }
---
+++ playground.js
@@ -0,0 +1,7 @@
+/*first show the guy*/
+
+/*mouse over to bounce him*/
+
+/*click to throu the thing (sound)*/
+
+/*x to strike a cool pose (sound)*/
